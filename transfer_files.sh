f661163bb82e8cc0a8da37acec02a174e1c68ac1
Christoffer Fjord Jørgensen <201805410@post.au.dk>
2022-12-25 14:16:00 +0100

    tests

--- transfer_files.sh
+++ transfer_files.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-# scp task_1_2/rest-server.py pi@192.168.0.101:worker/task12
-scp task_1_2/storage-node.py pi@192.168.0.101:worker/task12
-# scp task_1_2/lead-node.py pi@192.168.0.101:worker/task12
-# scp task_1_2/messages_pb2.py pi@192.168.0.101:worker/task12
-# scp database_methods.py pi@192.168.0.101:worker/task12
+# scp rest-server.py pi@192.168.0.101:worker/task21
+# scp storage-node.py pi@192.168.0.101:worker/task21
+# scp messages_pb2.py pi@192.168.0.101:worker/task21
+# scp database_methods.py pi@192.168.0.101:worker/task21
+# scp utils.py pi@192.168.0.101:worker/task21
+# scp reedsolomon.py pi@192.168.0.101:worker/task21
 
-scp task_1_2/storage-node.py pi@192.168.0.102:worker/task12
-# scp task_1_2/messages_pb2.py pi@192.168.0.102:worker/task12
-scp task_1_2/storage-node.py pi@192.168.0.103:worker/task12
-# scp task_1_2/messages_pb2.py pi@192.168.0.103:worker/task12
-scp task_1_2/storage-node.py pi@192.168.0.104:worker/task12
-# scp task_1_2/messages_pb2.py pi@192.168.0.104:worker/task12
+
+# scp storage-node.py pi@192.168.0.102:worker/task21
+# scp messages_pb2.py pi@192.168.0.102:worker/task21
+scp utils.py pi@192.168.0.102:worker/task21
+
+# scp storage-node.py pi@192.168.0.103:worker/task21
+# scp messages_pb2.py pi@192.168.0.103:worker/task21
+scp utils.py pi@192.168.0.103:worker/task21
+
+# scp storage-node.py pi@192.168.0.104:worker/task21
+# scp messages_pb2.py pi@192.168.0.104:worker/task21
+scp utils.py pi@192.168.0.104:worker/task21
